abfaace0a14bc9aac1507f91d2834fa760cdd7c2
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-03-17 20:54:02 +0100

    added button css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className="App">
       <div>
         <h1 className='text-6xl text-red-500'>Hello, Teste Tailwind</h1>
+        <button className='btn btn-blue'>Click me </button>
       </div>
     </div>
   );
