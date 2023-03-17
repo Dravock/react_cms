a4505e1d2b65688d6625ec5b092ffdd71ea7a2d9
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-03-17 21:03:32 +0100

    added readME

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <div>
         <h1 className='text-6xl text-red-500'>Hello, Teste Tailwind</h1>
-        <button className='btn btn-blue'>Click me </button>
+        <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full' >Click me </button>
       </div>
     </div>
   );
