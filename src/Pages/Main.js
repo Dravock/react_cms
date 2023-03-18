3c1a2205eff4fa35ba0f2dec619c633f47d03946
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-03-18 02:04:54 +0100

    PHP debugg read me erweitert

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
+
+
 function Main() {
 
 // REACT COMPONENT FUNKTIONS AND LOGIC
-
+const start_cms =()=>{
+    
+}
 
 // JSX RETURN BLOCK
 return (
     <div className="App">
-        <div>
-            <h1 className='text-6xl text-red-500'>Hello, Teste Tailwind</h1>
-            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full' >Click me </button>
+        <div className='text-center m-20'>
+            <h1 className='text-6xl text-red-500 m-32'>React ContentManagment System</h1>
+            <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full' onClick={()=>start_cms()}>Click to Start </button>
         </div>
     </div>
 )}
